4c5c8f824204ab816ce3eddf59697dedb66a4cbb
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-01-31 00:10:06 +0500

    Six Task added

---
+++ Task 6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите любое трёхзначное число: ");
+int number = int.Parse (Console.ReadLine());
+if (number>=100 && number<=999)
+{
+    number = number/10;
+    Console.WriteLine(number%10);
+}
