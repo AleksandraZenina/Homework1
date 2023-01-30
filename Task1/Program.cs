2b44ac770e55b302cfde9f6412da7ebca7825424
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-01-30 22:58:04 +0500

    First Task added

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number2==number1*number1)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Это просто два числа");
+}
