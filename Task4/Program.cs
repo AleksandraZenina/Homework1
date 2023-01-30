2c42e13f6f726d5ccf713c9896815c1cf18ca97e
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-01-30 23:43:59 +0500

    Fourth Task added

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать четные числа от 1 до N
+int count = 2;
+int N = new Random().Next(1,100);
+Console.WriteLine("Случайное число: " + N);
+while (count<=N)
+{
+    Console.WriteLine(count);
+    count = count + 2;
+}
+
