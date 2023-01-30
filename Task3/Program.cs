75400614fc88ae800e1b72ca031918d9b4ff9c33
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-01-30 23:26:11 +0500

    Third Task added

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число четным
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number%2==0)
+{
+    Console.WriteLine("Введенное число четное");
+}
+else
+{
+    Console.WriteLine("Введенное число нечетное");
+}
