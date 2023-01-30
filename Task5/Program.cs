994382e9ca13d1ad70c0510fefb8d354dc6dd65d
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-01-31 00:02:36 +0500

    Five Task added

---
+++ Task5/Program.cs
@@ -0,0 +1,7 @@
+﻿// показать последнюю цифру трёхзначного числа
+Console.Write("Введите любое трёхзначное число: ");
+int number = int.Parse (Console.ReadLine());
+if (number>=100 && number<=999)
+{
+    Console.WriteLine(number%10);
+}
