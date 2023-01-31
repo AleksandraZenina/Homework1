10338f4591deb0647e8fb8433d570f05235dde1a
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-01-31 22:56:49 +0500

    10th Task added

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number/100>1)
+{
+    Console.WriteLine((number/100)%10);
+}
+else
+{
+    Console.WriteLine("Введенное число меньше 100 и у него нет третьей цифры");
+}
