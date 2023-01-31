3d39fe9043e7f3dd9b3cf0d982a6ad73a00e2fe8
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-01-31 21:37:23 +0500

    Seventh Task added

---
+++ Task7/Program.cs
@@ -0,0 +1,21 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
+//int number = new Random().Next(10,100);
+//Console.Write("Случайное число: "+ number);//почему-то не соблюдается числовой отрезок 
+//при запуске генератора случайных чисел (выдаёт трехзачные числа)
+Console.Write("Введите любое число от 10 до 99: ");
+int number = int.Parse(Console.ReadLine());
+if (number>=10&&number<=99) 
+{
+  if (number/10>number%10)
+  {
+    Console.WriteLine(number/10);
+  }
+  else
+  {
+    Console.WriteLine(number%10);
+  }
+}
+else
+{
+    Console.WriteLine("Введено неверное значение");
+}
