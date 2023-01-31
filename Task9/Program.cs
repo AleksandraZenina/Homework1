ef3240f79311bb9b2dcc370eb0eccd8d160692fe
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-01-31 22:05:42 +0500

    9th Task added

---
+++ Task9/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+int a = 5;
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number%a==0)
+{
+    Console.WriteLine("Данное число кратно 5");
+}
+else
+{
+    Console.WriteLine(number%a);
+}
