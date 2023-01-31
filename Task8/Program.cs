f71b49061dafff8f3f2b269ac31b23d3a54016b1
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-01-31 21:49:54 +0500

    8th Task added

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.Write("Введите любое трёхзначное число: ");
+int number = int.Parse (Console.ReadLine());
+if (number>99&&number<=999)
+{
+    Console.Write(number/100);
+    Console.Write(number%10);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трёхзначное число!");
+}
